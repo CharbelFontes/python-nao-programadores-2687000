19d797ad9c38e7ee5ebc943cbfc8cbe58aead7e5
CharbelFontes <charbel.fontes@gmail.com>
2025-01-31 00:13:09 +0000

    confimação de atividade

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -2,10 +2,14 @@
 # - Iniciar sempre com letra minúscula;
 # - O nome pode ser composto por letras, algarismos e underlines;
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
-
+nome = 'Chárbel'
 # Declare variáveis do tipo string
-
+objetivo = ''' Aprender 
+linguagem
+de
+programação'''
 # Declare variáveis de cada tipo numérico
-
-
+numeroprimo = 1
+tamanhoMC = 29,9
 # Declare uma variável do tipo booleano
+Queromudardeprofissao = True
