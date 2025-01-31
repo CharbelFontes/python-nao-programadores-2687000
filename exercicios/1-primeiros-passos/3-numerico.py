a4a991f40459754ad65d25b977cfa31a45cec104
CharbelFontes <charbel.fontes@gmail.com>
2025-01-31 00:55:59 +0000

    exercicios

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+f'Paloma tem {2-1} dias'
 
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade_numerica + 2.4
+print (soma)
+print(int(soma))
+round(soma,0)
